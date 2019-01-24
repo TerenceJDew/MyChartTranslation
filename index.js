1f09db40e2727a2918cc4d60f86c57ae34aff374
Solad <SOLADIYI@ynhh.org>
2019-01-24 16:38:43 -0500

    Working Base64 encoding

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ const Handlebars = require('handlebars');
 const koaBody = require('koa-body')
 const translationAPI = require ('./translate.js')
 const Iog = require('iog');
+var Base64 = require('js-base64').Base64;
+var b64 = require('base-64');
+// var utf8 = require('to-utf-8');
+var iso88598i = require('iso-8859-8-i');
 // var fs = require('fs');
 // var enforceHttps = require('koa-sslify');
+// var decrypter = require('aes-decrypter').Decrypter;
  
 
 const app = new Koa();
@@ -25,7 +30,8 @@ const logger = new Iog('my-module-name');
 var appData = {
  "translationSource" : " ",
  "translationResults" : " ",
- "SourceLanguage": ""
+ "SourceLanguage": "",
+ "RequestURL": ""
 }
 
 const config = {
@@ -52,16 +58,54 @@ router.get ('api','/api/:text', async (ctx,next) => {
 
 } )
 
+router.get ('translate','/translate', async (ctx,next) => { 
+ log (`/translate endpoint without params`)
+ log (`Request Header`)
+ log (ctx.request.header)
+ ctx.body = "Empty /translate/ endpoint"
+
+} )
+
+
+router.get ('encrypt','/encrypt/', async (ctx,next) => { 
+  log (`/encrypt endpoint without params`)
+  log (`Request Header`)
+  log (ctx.request.params)
+  log (ctx.request.header)
+  log (ctx.request.query)
+  ctx.body = "Empty /encrypt/ endpoint"
+ 
+ } )
+
   router.get ('translate','/translate/:text', async (ctx,next) => {
-    try {log (`Request received`)
-  
-    appData.translationSource = decodeURIComponent((ctx.params.text + '').replace(/\+/g, '%20')); 
+    try {
+    log (`Request received`)
+    // log (`Request params: ${ctx.url}`)
+    log (`Text to Decode:' ${ctx.params.text}`)
+    // let content2 = Base64.decode(ctx.params.text);
+    let decoded = b64.decode (ctx.params.text);
+    log (`Decoded text: ${decoded}`);
+    // let decodedTxt = iso88598i.decode (decoded)
+    // log (`UTF8 text: ${decodedTxt}`);
+    // log (ctx.params);
+    // log (ctx.params.text)
+    // log (`Request Header`)
+    // log (ctx.request.header)
     
-    let content = JSON.stringify ([{'Text' : appData.translationSource}]);
+    appData.translationSource = decoded  
+    // appData.translationSource = decodeURIComponent((ctx.params.text + '').replace(/\+/g, '%20')); 
     
+    // let content = JSON.stringify ([{'Text' : appData.translationSource}]);
+    let content = JSON.stringify ([{'Text' : decoded}]);
+    // log (`Content: ${content}`)
+    // let content2 = Base64.decode(content);
+    // let content3 = iso88598i.decode (content2);
+    // log (`Translated content: ${content2}`)
+    // log (`Translated content3: ${content3}`)
     let newText =  await translationAPI.Translate(content)
     appData.translationResults = newText[0].translations[0].text
     appData.SourceLanguage=newText[0].detectedLanguage.language
+    appData.RequestURL= decoded
     // console.log (newText[0])
     
     ctx.body = await pageGenerator ()
@@ -120,7 +164,8 @@ async function pageGenerator () {
             <strong><h3>Translation:</h3></strong> {{translationResults}} </aside><!-- .right-sidebar -->
       
         </div><!-- .middle-->
-        <h5 id="discl"> This is computer generated translation. Please use only for as a reference   </h5>
+        
+        <h5 id="discl"> This is computer generated translation. Please use only as a reference   </h5>
       </div><!-- .wrapper -->
       
       <footer class="footer">
